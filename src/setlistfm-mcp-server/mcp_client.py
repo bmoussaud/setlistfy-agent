2ffd421924f204c617c26869ca3fe4c7119a89b1
Benoit Moussaud <benoit@moussaud.org>
2025-08-28 13:32:25 +0000

    fix: update server types in mcp.json from SSE to HTTP; add tests for MCP client functionality in mcp_client.py

--- src/setlistfm-mcp-server/mcp_client.py
+++ src/setlistfm-mcp-server/mcp_client.py
@@ -21,38 +21,36 @@ API_URL = os.getenv(
     "SETLISTFM_MCP_URL", "http://localhost:9001/sse")
 
 
-API_URL = "https://setlistfyagent-api-management-dev.azure-api.net/setlistfm-mcp/mcp"
+API_URL = "https://api.githubcopilot.com/mcp/"
 
 
-@pytest.mark.asyncio
-async def test_list_tools(caplog):
+async def test_list_tools():
     """Test the MCP root endpoint for FastMCP server."""
-    caplog.set_level(logging.INFO)
-    async with Client(API_URL) as client:
+    async with Client(API_URL, auth="oauth") as client:
         tools = await client.list_tools()
         assert isinstance(tools, list), "Expected tools to be a list"
         # logging.info("Available tools: %s", tools)
 
         for tool in tools:
             logging.info(f"Tool 1: {tool.name}")
-            logging.info(f"Tool 2: {tool.description}")
+            # logging.info(f"Tool 2: {tool.description}")
             assert tool.name, "Resource name should not be empty"
             assert tool.description, "Resource description should not be empty"
         assert len(
             tools) == 14, f"{len(tools)} resources should be available, check the server configuration"
-    # Print captured log output for pytest
-    print("\n".join(caplog.messages))
 
 
-@pytest.mark.asyncio
-async def test_call_tool(caplog):
-    """Test the MCP root endpoint for FastMCP server."""
-    caplog.set_level(logging.INFO)
+async def test_call_tool():
     async with Client(API_URL) as client:
         tool_name = "getArtist"
         tool_name = "returnsAnArtistForAGivenMusicbrainzMbid"
-        # Call the tool with parameters
+        logger.info(f"Calling tool: {tool_name}")
         response = await client.call_tool(tool_name, {"mbid": "b10bbbfc-cf9e-42e0-be17-e2c3e1d2600d"})
         assert response, "Response should not be empty"
         logging.info(f"Response: {response}")
-        assert "Beatles" in response[0].text, "Response should contain 'Beatles' key"
+        for item in response:
+            logging.info(f"Item: {item.text}")
+
+if __name__ == "__main__":
+    asyncio.run(test_list_tools())
+    # asyncio.run(test_call_tool())
